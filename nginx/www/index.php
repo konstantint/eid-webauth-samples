8cf5fbd0bfdfc3cd3125d757f9eb22fae3e8cc18
Konstantin Tretyakov <kt@ut.ee>
2016-01-15 02:03:54 +0100

    Added missing file

---
+++ nginx/www/index.php
@@ -0,0 +1,9 @@
+<html>
+<body>
+<b>Hello! The server reports the following variable values:</b>
+<pre>
+SSL_CLIENT_S_DN=<?=$_SERVER["SSL_CLIENT_S_DN"]?><br>
+SSL_CLIENT_VERIFY=<?=$_SERVER["SSL_CLIENT_VERIFY"]?>
+</pre>
+</body>
+</html>
